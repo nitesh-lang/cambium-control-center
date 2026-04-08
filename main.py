def6bb210614e29017bbeffcdf284aaa44b4b64e
Nitesh <nitesh@cambiumretail.com>
2026-04-08 15:54:05 +0530

    Fix async route, group tools by type, add blinkit badge

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-def home(request: Request):
+async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
